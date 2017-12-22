23ef8c320f08551c73de91f1b8df63b9c8484da2
André Laurentino <xor2017@outlook.com>
2017-12-22 13:38:25 -0200

    Segue o resto da views
    CadDestaqueArena é o crud que mencionei que esqueci de citar que tem que criar, nele vai ter um select onde seleciona rebaixados e som automotivo

---
+++ front-painel/pages/viewUsuarios.php
@@ -0,0 +1,41 @@
+<div id="page-wrapper">
+    <div class="graphs">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="container demo">
+                      <table class="datatable table table-striped table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Nome</th>
+                            <th>Função</th>
+                            <th>E-mail</th>                           
+                            <th>Foto</th>
+                            <th>Ações</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr class="gradeX">
+                            <td>xxx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>Win 95+</td>
+                            <td class="center">Xxxxxx</td>
+                            <td class="center">
+                                <a class="btn btn-success" href="">                                   
+                                    VISUALIZAR MAIS DETALHES
+                                </a>
+                                <a class="btn btn-primary" href="">                                    
+                                    EDITAR
+                                </a>
+                                <a class="btn btn-danger" href="">                                   
+                                    EXCLUIR
+                                </a>
+                            </td>
+                          </tr>                         
+                        </tbody>                        
+                      </table>
+                    </div><!--/.container.demo -->
+                </div>                
+            </div>
+        </div><!--/container-->
+    </div>
