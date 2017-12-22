41e03512b478354364bda68aa2de180f27a3eef7
André Laurentino <xor2017@outlook.com>
2017-12-22 13:19:16 -0200

    Views notícias e galeria

---
+++ front-painel/pages/viewNoticias.php
@@ -0,0 +1,42 @@
+<div id="page-wrapper">
+    <div class="graphs">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="container demo">
+                      <table class="datatable table table-striped table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Título</th>
+                            <th>Data</th>
+                            <th>Descrição</th>
+                            <th>Foto</th>
+                            <th>Ações</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr class="gradeX">
+                            <td>xxx</td>
+                            <td>xx/xx/xxxx</td>
+                            <td>Win 95+</td>
+                            <td class="center">Xxxxxx</td>
+                            <td class="center">
+                                <a class="btn btn-success" href="">                                   
+                                    VISUALIZAR
+                                </a>
+                                <a class="btn btn-primary" href="">                                    
+                                    EDITAR
+                                </a>
+                                <a class="btn btn-danger" href="">                                   
+                                    EXCLUIR
+                                </a>
+                            </td>
+                          </tr>                         
+                        </tbody>                        
+                      </table>
+                    </div><!--/.container.demo -->
+                </div>                
+            </div>
+        </div><!--/container-->
+    </div>
+</div>
