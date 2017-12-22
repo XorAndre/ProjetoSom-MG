45f15fe810c0567c9a4a134300ff6065a1ac7bd4
André Laurentino <xor2017@outlook.com>
2017-12-22 12:50:50 -0200

    Arquivo cadastro destaques Arena Som-mg

---
+++ front-painel/pages/CadDestaquesArena.php
@@ -0,0 +1,36 @@
+<div id="page-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="" method="">
+                    <div class="form-group">
+                        <label for="thumb">FOTO APRESENTAÇÃO</label>
+                        <input type="file" class="form-control1" id="thumb">
+                    </div>                    
+                    <div class="form-group">
+                        <input type="text" class="form-control1" id="focusedinput" placeholder="NOME">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control1" id="focusedinput" placeholder="DATA">
+                    </div>
+                    <div class="form-group">
+                        <label id="categoria"></label>
+                        <select class="form-control1" for="categoria">
+                            <option>REBAIXADO</option>
+                            <option>SOM AUTOMOTIVO</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control1" id="focusedinput" placeholder="DETALHES">
+                    </div>
+                     <div class="form-group">
+                        <input type="submit" class="btn btn-success" name="" value="CADASTRAR">
+                     </div>
+                     <div class="form-group">
+                        <input type="reset" class="btn btn-primary" value="CANCELAR">
+                     </div>
+                </form>
+            </div>
+        </div>
+    </div>    
+</div>
