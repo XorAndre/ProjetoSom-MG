979e9a27457feee0648c7118fd7b5ba8968c4ab7
André Laurentino <xor2017@outlook.com>
2017-12-28 11:10:04 -0200

    Header atual

---
+++ siteSomMG/header.php
@@ -0,0 +1,12 @@
+
+<header class="topo">
+    <div class="lang">
+        <form action="" method="post" class="selecione">
+            <label for="traduz">LANGUAGES</label>
+            <select name="" id="traduz" class="linguas">
+                <option value="">EN</option>
+                <option value="">PT</option>
+            </select>
+        </form>
+    </div><!--/lang-->    
+</header><!--/Header-->
